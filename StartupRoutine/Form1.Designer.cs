c3f778cc8932aa2e81fd2e15f29872782aca9eeb
Gergő Szabó <lest12110@gmail.com>
2022-10-12 16:28:20 +0200

    Add project files.

---
+++ StartupRoutine/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace BootRoutine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.runroutine = new System.Windows.Forms.Button();
+            this.routinelist = new System.Windows.Forms.ComboBox();
+            this.tasklist = new System.Windows.Forms.CheckedListBox();
+            this.newroutine = new System.Windows.Forms.Button();
+            this.editroutine = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.newroutinename = new System.Windows.Forms.TextBox();
+            this.saveroutine = new System.Windows.Forms.Button();
+            this.remove_tasks = new System.Windows.Forms.Button();
+            this.newtask = new System.Windows.Forms.Button();
+            this.removeroutine = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Still working";
+            this.notifyIcon1.BalloonTipTitle = "Still working";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Still working";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // runroutine
+            // 
+            this.runroutine.Location = new System.Drawing.Point(525, 12);
+            this.runroutine.Name = "runroutine";
+            this.runroutine.Size = new System.Drawing.Size(75, 23);
+            this.runroutine.TabIndex = 0;
+            this.runroutine.Text = "Run";
+            this.runroutine.UseVisualStyleBackColor = true;
+            this.runroutine.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // routinelist
+            // 
+            this.routinelist.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.routinelist.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.routinelist.FormattingEnabled = true;
+            this.routinelist.Location = new System.Drawing.Point(12, 12);
+            this.routinelist.MaxDropDownItems = 20;
+            this.routinelist.Name = "routinelist";
+            this.routinelist.Size = new System.Drawing.Size(121, 21);
+            this.routinelist.TabIndex = 2;
+            this.routinelist.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tasklist
+            // 
+            this.tasklist.FormattingEnabled = true;
+            this.tasklist.Location = new System.Drawing.Point(139, 12);
+            this.tasklist.Name = "tasklist";
+            this.tasklist.Size = new System.Drawing.Size(380, 184);
+            this.tasklist.TabIndex = 3;
+            this.tasklist.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // newroutine
+            // 
+            this.newroutine.Location = new System.Drawing.Point(12, 39);
+            this.newroutine.Name = "newroutine";
+            this.newroutine.Size = new System.Drawing.Size(121, 23);
+            this.newroutine.TabIndex = 4;
+            this.newroutine.Text = "New";
+            this.newroutine.UseVisualStyleBackColor = true;
+            this.newroutine.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // editroutine
+            // 
+            this.editroutine.Location = new System.Drawing.Point(12, 68);
+            this.editroutine.Name = "editroutine";
+            this.editroutine.Size = new System.Drawing.Size(121, 23);
+            this.editroutine.TabIndex = 5;
+            this.editroutine.Text = "Edit";
+            this.editroutine.UseVisualStyleBackColor = true;
+            this.editroutine.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // newroutinename
+            // 
+            this.newroutinename.Location = new System.Drawing.Point(12, 71);
+            this.newroutinename.Name = "newroutinename";
+            this.newroutinename.Size = new System.Drawing.Size(121, 20);
+            this.newroutinename.TabIndex = 6;
+            this.newroutinename.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // saveroutine
+            // 
+            this.saveroutine.Location = new System.Drawing.Point(12, 97);
+            this.saveroutine.Name = "saveroutine";
+            this.saveroutine.Size = new System.Drawing.Size(121, 23);
+            this.saveroutine.TabIndex = 7;
+            this.saveroutine.Text = "Save";
+            this.saveroutine.UseVisualStyleBackColor = true;
+            this.saveroutine.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // remove_tasks
+            // 
+            this.remove_tasks.Location = new System.Drawing.Point(12, 97);
+            this.remove_tasks.Name = "remove_tasks";
+            this.remove_tasks.Size = new System.Drawing.Size(121, 23);
+            this.remove_tasks.TabIndex = 8;
+            this.remove_tasks.Text = "Remove selected";
+            this.remove_tasks.UseVisualStyleBackColor = true;
+            this.remove_tasks.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // newtask
+            // 
+            this.newtask.Location = new System.Drawing.Point(12, 126);
+            this.newtask.Name = "newtask";
+            this.newtask.Size = new System.Drawing.Size(121, 23);
+            this.newtask.TabIndex = 9;
+            this.newtask.Text = "Add path";
+            this.newtask.UseVisualStyleBackColor = true;
+            this.newtask.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // removeroutine
+            // 
+            this.removeroutine.Location = new System.Drawing.Point(12, 173);
+            this.removeroutine.Name = "removeroutine";
+            this.removeroutine.Size = new System.Drawing.Size(121, 23);
+            this.removeroutine.TabIndex = 10;
+            this.removeroutine.Text = "Delete routine";
+            this.removeroutine.UseVisualStyleBackColor = true;
+            this.removeroutine.Click += new System.EventHandler(this.removeroutine_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(525, 173);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 11;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 211);
+            this.ControlBox = false;
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.removeroutine);
+            this.Controls.Add(this.newtask);
+            this.Controls.Add(this.remove_tasks);
+            this.Controls.Add(this.saveroutine);
+            this.Controls.Add(this.newroutinename);
+            this.Controls.Add(this.editroutine);
+            this.Controls.Add(this.newroutine);
+            this.Controls.Add(this.tasklist);
+            this.Controls.Add(this.routinelist);
+            this.Controls.Add(this.runroutine);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BootRoutine";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button runroutine;
+        private System.Windows.Forms.ComboBox routinelist;
+        private System.Windows.Forms.CheckedListBox tasklist;
+        private System.Windows.Forms.Button newroutine;
+        private System.Windows.Forms.Button editroutine;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox newroutinename;
+        private System.Windows.Forms.Button saveroutine;
+        private System.Windows.Forms.Button remove_tasks;
+        private System.Windows.Forms.Button newtask;
+        private System.Windows.Forms.Button removeroutine;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ StartupRoutine/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Collections;
+using System.Text;
+using System.Globalization;
+using System.Xml;
+using GemCard;
+using SmartCardPlayer;
+using System.Diagnostics;
+using System.Threading;
+
+namespace BootRoutine
+{
+
+    public partial class Form1 : Form
+    {
+        string CurrentPath = "";
+        void ptf(string path, string text, bool del = true)
+        {
+            if (File.Exists(path) && del)
+            {
+                File.Delete(path);
+                File.Create(path);
+            }
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.Write(text);
+            }
+        }
+        bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+            return false;
+        }
+        void RefreshCheckList()
+        {
+            if (routinelist.SelectedItem.ToString() != "")
+            {
+                tasklist.Items.Clear();
+                {
+                    int i = 0;
+                    while (IsFileLocked(new FileInfo(CurrentPath)))
+                    {
+                        i++;
+                        Thread.Sleep(10);
+                        if (i > 50)
+                            RefreshComboBox();
+                        return;
+                    }
+                }
+                string[] allines = File.ReadAllLines(CurrentPath);
+                foreach (string line in allines)
+                    tasklist.Items.Add(line);
+                for (int i = 0; i < tasklist.Items.Count; i++)
+                {
+                    tasklist.SetItemChecked(i, true);
+                }
+            }
+            else
+                tasklist.Items.Clear();
+        }
+        void RefreshComboBox()
+        {
+            routinelist.SelectedText = string.Empty;
+            routinelist.Hide();
+            Thread.Sleep(500);
+            routinelist.Items.Clear();
+            string[] filePaths = Directory.GetFiles("data");
+            if (filePaths.Length == 0)
+            {
+                return;
+            }
+            for (int i = 0; i < filePaths.Length; i++)
+            {
+                routinelist.Items.Add(filePaths[i].Split('\\')[1].Split('.')[0]);
+            }
+            routinelist.Show();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            newroutine.Show();
+            editroutine.Hide();
+            saveroutine.Hide();
+            remove_tasks.Hide();
+            removeroutine.Hide();
+            newtask.Hide();
+            newroutinename.Hide();
+            if (!Directory.Exists("data"))
+            {
+                Directory.CreateDirectory("data");
+            }
+            RefreshComboBox();
+        }
+        public static void startprocess(string path, string args = "")
+        {
+            Process ExternalProcess = new Process();
+            ExternalProcess.StartInfo.FileName = path;
+            ExternalProcess.StartInfo.Arguments = args;
+            ExternalProcess.Start();
+            ExternalProcess.WaitForExit();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> tasks = new List<string>();
+            for (int i = 0; i <= (tasklist.Items.Count - 1); i++)
+            {
+                if (tasklist.GetItemChecked(i))
+                {
+                    tasks.Add((tasklist.Items[i]).ToString());
+                }
+            }
+            List<Thread> procs = new List<Thread>();
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                procs.Add(new Thread(() => startprocess(tasks[i])));
+                procs[i].Start();
+            }
+            notifyIcon1.Visible = true;
+            Hide();
+            
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            notifyIcon1.Visible = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CurrentPath = "data/" + routinelist.SelectedItem + ".wsr";
+            RefreshCheckList();
+            editroutine.Show();
+            removeroutine.Show();
+            saveroutine.Hide();
+            remove_tasks.Hide();
+            newtask.Hide();
+            newroutinename.Hide();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < tasklist.Items.Count; i++)
+            {
+                tasklist.SetItemChecked(i, false);
+            }
+            remove_tasks.Show();
+            newtask.Show();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            newroutinename.Show();
+            removeroutine.Hide();
+            editroutine.Hide();
+            saveroutine.Hide();
+            remove_tasks.Hide();
+            newtask.Hide();
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (newroutinename.Text != "")
+            {
+                saveroutine.Show();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string fileName = "data/" + newroutinename.Text + ".wsr";
+            if (File.Exists(fileName))
+                System.Windows.Forms.MessageBox.Show("This routine already exists");
+            else
+                System.IO.File.Create("data/" + newroutinename.Text + ".wsr");
+            RefreshComboBox();
+            newroutine.Show();
+            editroutine.Hide();
+            saveroutine.Hide();
+            remove_tasks.Hide();
+            removeroutine.Hide();
+            newtask.Hide();
+            newroutinename.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            List<string> dat = new List<string>();
+            string s = "";
+            for (int i = 0; i <= (tasklist.Items.Count - 1); i++)
+            {
+
+                if (!tasklist.GetItemChecked(i))
+                {
+                    dat.Add(tasklist.Items[i].ToString());
+                    s += tasklist.Items[i].ToString() + "\n";
+                }
+            }
+
+            File.WriteAllText(CurrentPath, s);
+            RefreshCheckList();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            try
+            {
+                string path = openFileDialog1.FileName;
+                int sisi = tasklist.Items.Count;
+                if (path == null || path == "openFileDialog1" || path == tasklist.Items[sisi-1])
+                {
+                    int t = 0;
+                    int s = 1 / t;
+                }
+                ptf(CurrentPath, path + "\n", false);
+            }
+            catch
+            {
+                DialogResult dialogResult = MessageBox.Show("Add path manually?", "Windows didn't like that", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    startprocess("notepad", Path.GetFullPath(CurrentPath));
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                }
+            }
+            //File.AppendAllText(CurrentPath, path+"\n");
+            RefreshCheckList();
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void removeroutine_Click(object sender, EventArgs e)
+        {
+            {
+                int i = 0;
+                while (IsFileLocked(new FileInfo(CurrentPath)))
+                {
+                    i++;
+                    Thread.Sleep(10);
+                    if (i > 50)
+                        return;
+                }
+            }
+            File.Delete(CurrentPath);
+            RefreshCheckList();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
